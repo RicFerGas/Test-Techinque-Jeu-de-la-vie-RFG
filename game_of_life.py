ea991f1f524b27d1a52618fef9a0b65873a006b8
Ricardo FG <ricardo.fdz.gasca@gmail.com>
2024-04-07 17:21:29 +0200

    Comments added

--- game_of_life.py
+++ game_of_life.py
@@ -6,9 +6,12 @@ from initial_seeds import seeds
 
 class GameOfLife:
     def __init__(self, initial_grid: List[List[bool]]):
+        # Initialize the grid
         self.grid = initial_grid
+        # Set the grid dimensions
         self.GRID_HEIGHT = len(initial_grid)
         self.GRID_WIDTH = len(initial_grid[0])
+        # Save the states of the game
         self.states = []
         self.states_count = 0
         #Save the initial state
@@ -21,26 +24,26 @@ class GameOfLife:
         for i in range(self.GRID_HEIGHT):
             for j in range(self.GRID_WIDTH):
                 live_neighbors = self.count_live_neighbors(i, j)
-                if self.grid[i][j] and live_neighbors in [2, 3]:
+                if self.grid[i][j] and live_neighbors in [2, 3]: # Any live cell with two or three live neighbors survives
                     new_grid[i][j] = True
-                elif not self.grid[i][j] and live_neighbors == 3:
+                elif not self.grid[i][j] and live_neighbors == 3: # Any dead cell with three live neighbors becomes a live cell
                     new_grid[i][j] = True
                 else:
-                    new_grid[i][j] = False
+                    new_grid[i][j] = False # All other live cells die in the next generation
         #Save the state
         self.states.append(new_grid)
-        self.grid = new_grid
+        self.grid = new_grid # Update the grid
 
     def count_live_neighbors(self, i: int, j: int) -> int:
         count = 0
-        for x in range(i - 1, i + 2):
-            for y in range(j - 1, j + 2):
-                if x == i and y == j:
+        for x in range(i - 1, i + 2): # Loop over the 3x3 neighborhood
+            for y in range(j - 1, j + 2): # Loop over the 3x3 neighborhood
+                if x == i and y == j: # Skip the current cell
                     continue
-                if 0 <= x < self.GRID_HEIGHT and 0 <= y < self.GRID_WIDTH:
-                    if self.grid[x][y]:
+                if 0 <= x < self.GRID_HEIGHT and 0 <= y < self.GRID_WIDTH: # Check the boundaries
+                    if self.grid[x][y]: # Check if the cell is alive
                         count += 1
-        return count
+        return count # Return the number of live neighbors
 
     def display_grid(self):
         os.system('cls' if os.name == 'nt' else 'clear')  # Clear the terminal screen
@@ -48,10 +51,11 @@ class GameOfLife:
         print(f"Generation: {self.states_count}")
         print("Press Ctrl+C to stop the simulation.")
         for row in self.grid:
-            print("".join(["[o]" if cell else "[ ]" for cell in row]))
+            print("".join(["[o]" if cell else "[ ]" for cell in row])) # Display the grid
         print()
 
 def write_game_states_to_file(output_document: str, game: GameOfLife) -> None:
+    # Write the game states to a file in the "Runned games" folder
     with open('Runned games/'+output_document, "w") as file:
         for i, state in enumerate(game.states):
             file.write(f"Generation: {i}\n")
@@ -60,38 +64,38 @@ def write_game_states_to_file(output_document: str, game: GameOfLife) -> None:
             file.write("\n")
 
 def read_configuration(filename: str):
-    """Reads the configuration from the given file and returns the extracted values."""
+    # Read the configuration file and return the parameters
     config: List[str] = []
     with open(filename, "r") as file:
         for line in file:
             line = line.split("#")[0].strip()
             if line: 
                 config.append(line)
-    num_generations: int = int(config[0].strip())
+    num_generations: int = int(config[0].strip()) 
     initial_seed_name: str = config[1].strip().strip('"')
-    oscillation_time: float = float(config[2].strip())
+    oscillation_time: float = float(config[2].strip()) 
     output_document: str = config[3].strip()
     return num_generations, initial_seed_name, oscillation_time, output_document, config
 
 def get_initial_seed(initial_seed_name: str, config: List[str]) -> List[List[int]]:
-    """Determines the initial seed based on its name or custom seed content."""
+    # Determines the initial seed based on its name or custom seed content.
     custom_seed: List[List[int]] = []
     if initial_seed_name.startswith("Custom"):
-        custom_seed_content: str = '\n'.join(config[4:]).strip()
+        custom_seed_content: str = '\n'.join(config[4:]).strip() # Get the custom seed content as a string
         custom_seed = eval(custom_seed_content)  # Evaluate the content as Python code
         initial_seed_name = "Custom"
     if initial_seed_name in seeds:
-        initial_seed = seeds[initial_seed_name]
+        initial_seed = seeds[initial_seed_name] # Get the initial seed from the seeds dictionary
     elif custom_seed:
-        initial_seed = custom_seed
+        initial_seed = custom_seed # Use the custom seed
     else:
         print("Error: Initial seed not found.")
         exit()
     return initial_seed
 
 def start_simulation(game: GameOfLife, num_generations: int, oscillation_time: float, output_document: str) -> None:
-    """Starts the simulation using the specified Game of Life object and parameters."""
-    game.display_grid()
+    # Starts the simulation using the specified Game of Life object and parameters.
+    game.display_grid() 
     print("Initial state:")
     ready = input("Press Enter to start the simulation")
     if ready == "":
@@ -101,7 +105,6 @@ def start_simulation(game: GameOfLife, num_generations: int, oscillation_time: f
                 game.evolve()
                 # Display the game grid
                 game.display_grid()
-                # Optionally, add some delay between generations
                 time.sleep(oscillation_time)
         except KeyboardInterrupt:
             write_game_states_to_file(output_document, game)
--- unit_tests.py
+++ unit_tests.py
@@ -5,16 +5,19 @@ from initial_seeds import seeds
 
 class TestGameOfLife(unittest.TestCase):
     def test_evolve(self):
+        # Do a iteration of an oscillator 1x3
         initial_grid: List[List[bool]] = [[False, True, False], [False, True, False], [False, True, False]]
         game = GameOfLife(initial_grid)
         game.evolve()
+        # Expected grid after one iteration of the oscillator
         expected_grid: List[List[bool]] = [[False, False, False], [True, True, True], [False, False, False]]
         self.assertEqual(game.grid, expected_grid)
 
     def test_count_live_neighbors(self):
+        #Test the count of live neighbors of a cell in the middle of the grid (1,1)
         initial_grid: List[List[bool]] = [[False, True, False], [False, True, False], [False, True, False]]
         game = GameOfLife(initial_grid)
-        self.assertEqual(game.count_live_neighbors(1, 1), 2)
+        self.assertEqual(game.count_live_neighbors(1, 1), 2) # Expected 2 live neighbors
 
 class TestReadConfiguration(unittest.TestCase):
     def test_read_configuration(self):
@@ -27,16 +30,18 @@ class TestReadConfiguration(unittest.TestCase):
 
 class TestGetInitialSeed(unittest.TestCase):
     def test_get_initial_seed_custom(self):
+        # Test a custom seed and config
         initial_seed_name = "Custom"
         config = ["", "", "", "", "[[1, 0], [0, 1]]"]
         expected_seed = [[1, 0], [0, 1]]
-        self.assertEqual(get_initial_seed(initial_seed_name, config), expected_seed)
+        self.assertEqual(get_initial_seed(initial_seed_name, config), expected_seed) # Expected custom seed
     
     def test_get_initial_seed_known(self):
+        # Test a known seed and config
         initial_seed_name = "Glider"
         config = ["", "", "", "", "[[0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [1, 1, 1, 0, 0]]"]
         expected_seed = seeds["Glider"]
-        self.assertEqual(get_initial_seed(initial_seed_name, config), expected_seed)
+        self.assertEqual(get_initial_seed(initial_seed_name, config), expected_seed) # Expected known seed Glider
 
 
 if __name__ == "__main__":
